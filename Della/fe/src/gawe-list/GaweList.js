cbab3ef7ef73ae1b3b405f65f723b14d4ff656bd
dellarizkyanaokvitaria <dellarizkyanaokvitaria@gmail.com>
2021-08-25 11:59:38 +0700

    setup react

---
+++ Della/fe/src/gawe-list/GaweList.js
@@ -0,0 +1,13 @@
+import './gaweList.css'
+
+const GaweList = () => {
+    return(
+        <ul>
+            <li>Belajar gawe final</li>
+            <li>Ojo Males</li>
+            <li>Ayo sinau</li>
+        </ul>
+    )
+}
+
+export default GaweList
