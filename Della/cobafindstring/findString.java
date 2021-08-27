8b8c78b3f9050343d9d7a99c45c4ba48ae032ca0
dellarizkyanaokvitaria <dellarizkyanaokvitaria@gmail.com>
2021-08-27 22:25:54 +0700

    lomba

---
+++ Della/cobafindstring/findString.java
@@ -0,0 +1,12 @@
+package cobafindstring;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String huruf = "ABCDCDC";
+        System.out.println(huruf);
+
+        System.out.println(huruf.substring(2));
+    }
+}
